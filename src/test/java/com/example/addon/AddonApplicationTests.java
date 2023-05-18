d645ad9ec194b6203bc298233d83381ce862008f
Omar Abdelmoneim Gamal <58474037+OmarAGamal@users.noreply.github.com>
2023-05-18 11:41:30 +0300

    Add files via upload

---
+++ src/test/java/com/example/addon/AddonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
